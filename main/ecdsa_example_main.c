e0840432e7eed0e39fea2224a3c25a32eeead198
Meng Hang <chocolate.menghang@gmail.com>
2022-05-06 15:45:44 +0800

    Update for function tests.
    Signed-off-by: Meng Hang <chocolate.menghang@gmail.com>

--- main/ecdsa_example_main.c
+++ main/ecdsa_example_main.c
@@ -41,25 +41,30 @@
 #include "mbedtls/ctr_drbg.h"
 #include "mbedtls/pk.h"
 
-static const char *TAG = "atecc_example";
+static const char *TAG = "ATECC608";
 /* globals for mbedtls RNG */
 static mbedtls_entropy_context entropy;
 static mbedtls_ctr_drbg_context ctr_drbg;
 
+#define ATECC608B_ADDR 0x38
+
 static int configure_mbedtls_rng(void)
 {
     int ret;
-    const char * seed = "some random seed string";
+    const char *seed = "some random seed string";
     mbedtls_ctr_drbg_init(&ctr_drbg);
 
     ESP_LOGI(TAG, "Seeding the random number generator...");
 
     mbedtls_entropy_init(&entropy);
     ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
-        (const unsigned char *)seed, strlen(seed));
-    if (ret != 0) {
+                                (const unsigned char *)seed, strlen(seed));
+    if (ret != 0)
+    {
         ESP_LOGI(TAG, " failed  ! mbedtls_ctr_drbg_seed returned %d", ret);
-    } else {
+    }
+    else
+    {
         ESP_LOGI(TAG, " ok");
     }
     return ret;
@@ -74,18 +79,16 @@ static void close_mbedtls_rng(void)
 /* An example hash */
 static unsigned char hash[32] = {
     0xba, 0x78, 0x16, 0xbf, 0x8f, 0x01, 0xcf, 0xea, 0x41, 0x41, 0x40, 0xde, 0x5d, 0xae, 0x22, 0x23,
-    0xb0, 0x03, 0x61, 0xa3, 0x96, 0x17, 0x7a, 0x9c, 0xb4, 0x10, 0xff, 0x61, 0xf2, 0x00, 0x15, 0xad
-};
+    0xb0, 0x03, 0x61, 0xa3, 0x96, 0x17, 0x7a, 0x9c, 0xb4, 0x10, 0xff, 0x61, 0xf2, 0x00, 0x15, 0xad};
 
 static const uint8_t public_key_x509_header[] = {
     0x30, 0x59, 0x30, 0x13, 0x06, 0x07, 0x2A, 0x86, 0x48, 0xCE, 0x3D, 0x02, 0x01, 0x06, 0x08, 0x2A,
-    0x86, 0x48, 0xCE, 0x3D, 0x03, 0x01, 0x07, 0x03, 0x42, 0x00, 0x04
-};
+    0x86, 0x48, 0xCE, 0x3D, 0x03, 0x01, 0x07, 0x03, 0x42, 0x00, 0x04};
 
 static void print_public_key(uint8_t *pubkey)
 {
     uint8_t buf[128];
-    uint8_t * tmp;
+    uint8_t *tmp;
     size_t buf_len = sizeof(buf);
 
     /* Calculate where the raw data will fit into the buffer */
@@ -98,7 +101,7 @@ static void print_public_key(uint8_t *pubkey)
     memcpy(tmp + sizeof(public_key_x509_header), pubkey, ATCA_PUB_KEY_SIZE);
 
     /* Convert to base 64 */
-    (void)atcab_base64encode(tmp, ATCA_PUB_KEY_SIZE + sizeof(public_key_x509_header), (char*)buf, &buf_len);
+    (void)atcab_base64encode(tmp, ATCA_PUB_KEY_SIZE + sizeof(public_key_x509_header), (char *)buf, &buf_len);
 
     /* Add a null terminator */
     buf[buf_len] = '\0';
@@ -118,28 +121,31 @@ static int atca_ecdsa_test(void)
 
 #ifdef MBEDTLS_ECDSA_SIGN_ALT
     /* Convert to an mbedtls key */
-    ESP_LOGI(TAG,  " Using a hardware private key ..." );
+    ESP_LOGI(TAG, "Using a hardware private key ...");
     ret = atca_mbedtls_pk_init(&pkey, 0);
-    if (ret != 0) {
-        ESP_LOGI(TAG, " failed !  atca_mbedtls_pk_init returned %02x", ret);
+    if (ret != 0)
+    {
+        ESP_LOGI(TAG, "failed !  atca_mbedtls_pk_init returned %02x", ret);
         goto exit;
     }
-    ESP_LOGI(TAG, " ok");
+    ESP_LOGI(TAG, "ok");
 #else
-    ESP_LOGI(TAG,  " Generating a software private key ..." );
+    ESP_LOGI(TAG, " Generating a software private key ...");
     mbedtls_pk_init(&pkey);
     ret = mbedtls_pk_setup(&pkey,
-            mbedtls_pk_info_from_type( MBEDTLS_PK_ECDSA ));
-    if (ret != 0) {
-        ESP_LOGI(TAG,  " failed !  mbedtls_pk_setup returned -0x%04x", -ret );
+                           mbedtls_pk_info_from_type(MBEDTLS_PK_ECDSA));
+    if (ret != 0)
+    {
+        ESP_LOGI(TAG, " failed !  mbedtls_pk_setup returned -0x%04x", -ret);
         goto exit;
     }
 
-    ret = mbedtls_ecp_gen_key( MBEDTLS_ECP_DP_SECP256R1,
-                                   mbedtls_pk_ec( pkey ),
-                                   mbedtls_ctr_drbg_random, &ctr_drbg );
-    if (ret != 0) {
-        ESP_LOGI(TAG,  " failed !  mbedtls_ecp_gen_key returned -0x%04x", -ret );
+    ret = mbedtls_ecp_gen_key(MBEDTLS_ECP_DP_SECP256R1,
+                              mbedtls_pk_ec(pkey),
+                              mbedtls_ctr_drbg_random, &ctr_drbg);
+    if (ret != 0)
+    {
+        ESP_LOGI(TAG, " failed !  mbedtls_ecp_gen_key returned -0x%04x", -ret);
         goto exit;
     }
     ESP_LOGI(TAG, " ok");
@@ -147,25 +153,50 @@ static int atca_ecdsa_test(void)
 
     ESP_LOGI(TAG, " Generating ECDSA Signature...");
     ret = mbedtls_pk_sign(&pkey, MBEDTLS_MD_SHA256, hash, 0, buf, &olen,
-        mbedtls_ctr_drbg_random, &ctr_drbg);
-    if (ret != 0) {
-        ESP_LOGI(TAG, " failed ! mbedtls_pk_sign returned -0x%04x", -ret);
+                          mbedtls_ctr_drbg_random, &ctr_drbg);
+    if (ret != 0)
+    {
+        ESP_LOGI(TAG, "failed ! mbedtls_pk_sign returned -0x%04x", -ret);
         goto exit;
     }
-    ESP_LOGI(TAG, " ok");
+    ESP_LOGI(TAG, "ok");
 
-    ESP_LOGI(TAG, " Verifying ECDSA Signature...");
+    ESP_LOGI(TAG, "Verifying ECDSA Signature...");
     ret = mbedtls_pk_verify(&pkey, MBEDTLS_MD_SHA256, hash, 0,
-        buf, olen);
-    if (ret != 0) {
-        ESP_LOGI(TAG, " failed ! mbedtls_pk_verify returned -0x%04x", -ret);
+                            buf, olen);
+    if (ret != 0)
+    {
+        ESP_LOGI(TAG, "failed ! mbedtls_pk_verify returned -0x%04x", -ret);
         goto exit;
     }
-    ESP_LOGI(TAG, " ok");
+    ESP_LOGI(TAG, "ok");
 
 exit:
     fflush(stdout);
     return ret;
+}
+
+static void print_config_zone(void)
+{
+    uint8_t buf[ATCA_ECC_CONFIG_SIZE];
+
+    ATCA_STATUS ret = atcab_read_config_zone(buf);
+
+    if (ATCA_SUCCESS != ret)
+    {
+        ESP_LOGI(TAG, "atcab_read_config_zone fail->0x%02X", ret);
+    }
+    else
+    {
+        ESP_LOGI(TAG, "Config zone->");
+        for (uint8_t ii = 0; ii < ATCA_ECC_CONFIG_SIZE; ii += 8)
+        {
+            ESP_LOGI(TAG, "%03d->0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X",
+                     ii,
+                     buf[ii], buf[ii + 1], buf[ii + 2], buf[ii + 3],
+                     buf[ii + 4], buf[ii + 5], buf[ii + 6], buf[ii + 7]);
+        }
+    }
 }
 
 void app_main(void)
@@ -177,64 +208,104 @@ void app_main(void)
 
     /* Initialize the mbedtls library */
     ret = configure_mbedtls_rng();
+
 #ifdef CONFIG_ATECC608A_TNG
-    ESP_LOGI(TAG, "  . Initialize the ATECC interface for Trust & GO ...");
+    // ESP_LOGI(TAG, "Initialize the ATECC interface for Trust & GO ...");
     cfg_ateccx08a_i2c_default.atcai2c.address = 0x6A;
-#elif CONFIG_ATECC608A_TFLEX /* CONFIG_ATECC608A_TNGO */
-    ESP_LOGI(TAG, "  . Initialize the ATECC interface for TrustFlex ...");
+#elif CONFIG_ATECC608A_TFLEX   /* CONFIG_ATECC608A_TNGO */
+    // ESP_LOGI(TAG, "Initialize the ATECC interface for TrustFlex ...");
     cfg_ateccx08a_i2c_default.atcai2c.address = 0x6C;
 #elif CONFIG_ATECC608A_TCUSTOM /* CONFIG_ATECC608A_TFLEX */
-    ESP_LOGI(TAG, "  . Initialize the ATECC interface for TrustCustom ...");
+    // ESP_LOGI(TAG, "Initialize the ATECC interface for TrustCustom ...");
+    cfg_ateccx08a_i2c_default.atcai2c.address = ATECC608B_ADDR << 1;
+    cfg_ateccx08a_i2c_default.atcai2c.baud = 400000;
     /* Default slave address is same as that of TCUSTOM ATECC608A chips */
-#endif /* CONFIG_ATECC608A_TCUSTOM */
+#endif                         /* CONFIG_ATECC608A_TCUSTOM */
     ret = atcab_init(&cfg_ateccx08a_i2c_default);
-    if (ret != 0) {
-        ESP_LOGI(TAG, " failed ! atcab_init returned %02x", ret);
+    if (ret != 0)
+    {
+        ESP_LOGI(TAG, "failed! atcab_init returned %02x", ret);
         goto exit;
     }
-    ESP_LOGI(TAG, " ok");
+    else
+    {
+        ESP_LOGI(TAG, "ok");
+    }
 
     lock = 0;
     ESP_LOGI(TAG, " Check the data zone lock status...");
     ret = atcab_is_locked(LOCK_ZONE_DATA, &lock);
-    if (ret != 0) {
-        ESP_LOGI(TAG, " failed\n  ! atcab_is_locked returned %02x", ret);
+    if (ret != 0)
+    {
+        ESP_LOGI(TAG, "failed\n  ! atcab_is_locked returned %02x", ret);
         goto exit;
     }
 
-    if (lock) {
-        ESP_LOGI(TAG, " ok: locked");
-    } else {
+    if (lock)
+    {
+        ESP_LOGI(TAG, "ok: locked");
+    }
+    else
+    {
         ESP_LOGE(TAG, "unlocked, please lock(configure) the ATECC608A chip with help of esp_cryptoauth_utility and try again");
         goto exit;
     }
 
-    ESP_LOGI(TAG, " Get the device info (type)...");
+    ESP_LOGI(TAG, "Get the device info (type)...");
     ret = atcab_info(buf);
-    if (ret != 0) {
-        ESP_LOGI(TAG, " failed\n  ! atcab_info returned %02x", ret);
+    if (ret != 0)
+    {
+        ESP_LOGI(TAG, "failed\n  ! atcab_info returned %02x", ret);
         goto exit;
     }
-    ESP_LOGI(TAG, " ok: %02x %02x", buf[2], buf[3]);
+    ESP_LOGI(TAG, "device info->0x%02X 0x%02X 0x%02X 0x%02X", buf[0], buf[1], buf[2], buf[3]);
 
-    ESP_LOGI(TAG, " Get the public key...");
-    ret = atcab_get_pubkey(0, pubkey);
-    if (ret != 0) {
-        ESP_LOGI(TAG, " failed\n  ! atcab_get_pubkey returned %02x", ret);
+    print_config_zone();
+
+    // ESP_LOGI(TAG, "Get the public key...");
+    // ret = atcab_get_pubkey(0, pubkey);
+    // if (ret != 0)
+    // {
+    //     ESP_LOGI(TAG, " failed\n  ! atcab_get_pubkey returned %02x", ret);
+    //     goto exit;
+    // }
+    // ESP_LOGI(TAG, "ok");
+    // print_public_key(pubkey);
+
+    // /* Perform a Sign/Verify Test */
+    // ret = atca_ecdsa_test();
+    // if (ret != 0)
+    // {
+    //     ESP_LOGE(TAG, " ECDSA sign/verify failed");
+    //     goto exit;
+    // }
+    uint8_t random[32] = {0};
+    ret = atcab_random(random);
+    if (ATCA_SUCCESS != ret)
+    {
+        ESP_LOGE(TAG, "Get random number fails");
         goto exit;
     }
-    ESP_LOGI(TAG, " ok");
-    print_public_key(pubkey);
-
-    /* Perform a Sign/Verify Test */
-    ret = atca_ecdsa_test();
-    if (ret != 0) {
-        ESP_LOGE(TAG, " ECDSA sign/verify failed");
+    else
+    {
+        ESP_LOGI(TAG, "Random num1->0x%08X 0x%08X 0x%08X 0x%08X 0x%08X 0x%08X 0x%08X 0x%08X",
+                 *(uint32_t *)random, *((uint32_t *)random + 1), *((uint32_t *)random + 2), *((uint32_t *)random + 3),
+                 *((uint32_t *)random + 4), *((uint32_t *)random + 5), *((uint32_t *)random + 6), *((uint32_t *)random + 7));
+    }
+    ret = atcab_random(random);
+    if (ATCA_SUCCESS != ret)
+    {
+        ESP_LOGE(TAG, "Get random number fails");
         goto exit;
+    }
+    else
+    {
+        ESP_LOGI(TAG, "Random num2->0x%08X 0x%08X 0x%08X 0x%08X 0x%08X 0x%08X 0x%08X 0x%08X",
+                 *(uint32_t *)random, *((uint32_t *)random + 1), *((uint32_t *)random + 2), *((uint32_t *)random + 3),
+                 *((uint32_t *)random + 4), *((uint32_t *)random + 5), *((uint32_t *)random + 6), *((uint32_t *)random + 7));
     }
 
 exit:
     fflush(stdout);
     close_mbedtls_rng();
-
 }

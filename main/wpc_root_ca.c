828a3e03e9f4111867c4084130272268adcafdd1
Meng Hang <chocolate.menghang@gmail.com>
2022-05-09 21:22:20 +0800

    Add root certificate (WPCCA1) in base64.
    Signed-off-by: Meng Hang <chocolate.menghang@gmail.com>

---
+++ main/wpc_root_ca.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+const char wpcca1_root_ca_base64[] =
+    "MIIBLDCB06ADAgECAgh3YRK0EUearDAKBggqhkjOPQQDAjARMQ8wDQYDVQQDDAZX\r\n"
+    "UENDQTEwIBcNMjEwMzAzMTYwNDAxWhgPOTk5OTEyMzEyMzU5NTlaMBExDzANBgNV\r\n"
+    "BAMMBldQQ0NBMTBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABASEByCzo7F50Gx3\r\n"
+    "raAARNLPIJvDPONEmgjGf4/1A3NjoEP1kHGQr7e88fymZ8tew+oZ+Wp7QR89jWMw\r\n"
+    "SC1+KBKjEzARMA8GA1UdEwEB/wQFMAMBAf8wCgYIKoZIzj0EAwIDSAAwRQIgacC3\r\n"
+    "FeLAavaI+bZHo49TZ9UCpvXeVbkFoQJavUJx9GgCIQCfX08/diCuF3TYBLxxmqGb\r\n"
+    "UFW7ou1N5kD9VPo5W+1duA==";
+
+const size_t wpcca1_root_ca_base64_size = sizeof(wpcca1_root_ca_base64) / sizeof(uint8_t);
---
+++ main/wpc_root_ca.h
@@ -0,0 +1,15 @@
+#ifndef WPC_ROOT_CA_H
+#define WPC_ROOT_CA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const char wpcca1_root_ca_base64[];
+extern const size_t wpcca1_root_ca_base64_size;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
